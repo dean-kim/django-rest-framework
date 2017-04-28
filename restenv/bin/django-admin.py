3c209a42f026517cf6e5679bad1fc6ead5dc1d7b
dean-kim <keunu07@gmail.com>
2017-04-28 15:55:53 +0900

    gitignore fixed

---
+++ restenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/KEUNU/study/rest_framework/django-rest-framework/restenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
